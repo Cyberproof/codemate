54cd0a72d841400dc0917cb5aca8110b1f89d9ee
David Meu <david.meushar@cyberproof.com>
2021-08-15 13:35:25 +0300

    adding version handling

--- codemate/__init__.py
+++ codemate/__init__.py
@@ -1,10 +1,11 @@
 """
 Python syntax generator based on Object-Oriented Programing, type hints, and simplicity.
 """
-
+from codemate._version import __version__
 from codemate.block import Block
 from codemate.file import File
 from codemate.method import ClassMethod, Method, StaticMethod
 from codemate.structure import Class, Function
 
-__version__ = "0.1.2"
+# Package version
+__version__ = ".".join(str(i) for i in __version__)  # type: ignore
